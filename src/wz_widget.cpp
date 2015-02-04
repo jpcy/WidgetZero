fbb4fecdac7a3d97a04f5b5b57174773d9e26439
Jonathan Young <young.jpc@gmail.com>
2015-02-04 21:56:46 +1000

    Move utility code to wz.cpp.

--- src/wz_widget.cpp
+++ src/wz_widget.cpp
@@ -21,34 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-/*
-Rect::intersect is SDL_IntersectRect
-
-  Simple DirectMedia Layer
-  Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-*/
 #include "wz_pch.h"
 #pragma hdrstop
 
 namespace wz {
-
-const Border Border_zero;
 
 WidgetChildren::~WidgetChildren()
 {
@@ -866,54 +842,6 @@ void WidgetImpl::resizeToMeasuredRecursive()
 	{
 		children[i]->resizeToMeasuredRecursive();
 	}
-}
-
-/*
-================================================================================
-
-RECTANGLES
-
-================================================================================
-*/
-
-bool Rect::intersect(const Rect A, const Rect B, Rect *result)
-{
-    int Amin, Amax, Bmin, Bmax;
-
-	WZ_ASSERT(result);
-
-    /* Special cases for empty rects */
-    if (A.isEmpty() || B.isEmpty())
-	{
-		result->x = result->y = result->w = result->h = 0;
-        return false;
-    }
-
-    /* Horizontal intersection */
-    Amin = A.x;
-    Amax = Amin + A.w;
-    Bmin = B.x;
-    Bmax = Bmin + B.w;
-    if (Bmin > Amin)
-        Amin = Bmin;
-    result->x = Amin;
-    if (Bmax < Amax)
-        Amax = Bmax;
-    result->w = Amax - Amin;
-
-    /* Vertical intersection */
-    Amin = A.y;
-    Amax = Amin + A.h;
-    Bmin = B.y;
-    Bmax = Bmin + B.h;
-    if (Bmin > Amin)
-        Amin = Bmin;
-    result->y = Amin;
-    if (Bmax < Amax)
-        Amax = Bmax;
-    result->h = Amax - Amin;
-
-    return !result->isEmpty();
 }
 
 /*
